9bf438a7682a19457a4bad73321e9d564b390d31
martinmolema <martin@molema.org>
2021-11-27 16:18:14 +0100

    Changed the datamodel: the product now belongs to the meter instead of telwerk

--- database/CreateDatabase.sql
+++ database/CreateDatabase.sql
@@ -1,126 +1,121 @@
--- phpMyAdmin SQL Dump
--- version 4.6.6deb5
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Gegenereerd op: 16 feb 2020 om 22:55
--- Serverversie: 5.7.29-0ubuntu0.18.04.1
--- PHP-versie: 7.2.24-0ubuntu0.18.04.2
+DROP TABLE IF EXISTS tbl_meters_standen;
+DROP TABLE IF EXISTS tbl_meter_telwerken;
+DROP TABLE IF EXISTS tbl_meters;
+DROP TABLE IF EXISTS tbl_klanten;
+DROP TABLE IF EXISTS tbl_adressen;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-DROP TABLE IF EXISTS tbl_adressen  ;
-DROP TABLE IF EXISTS tbl_klanten  ;
-DROP TABLE IF EXISTS tbl_meter_telwerken  ;
-DROP TABLE IF EXISTS tbl_meters  ;
-DROP TABLE IF EXISTS tbl_meters_standen  ;
-
+-- --------------------------------------------------------
 --
--- Database: `secriskyouthenergy`
+-- Tabelstructuur voor tabel tbl_adressen
 --
+
+CREATE TABLE tbl_adressen
+(
+    a_idAdres    int(11)      NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    a_plaatsnaam varchar(80)  NOT NULL,
+    a_gemeente   varchar(80)  NOT NULL,
+    a_provincie  varchar(80)  NOT NULL,
+    a_regio      varchar(80)  NOT NULL,
+    a_straatnaam varchar(200) NOT NULL,
+    a_huisnummer varchar(16)  NOT NULL,
+    a_postcode   varchar(12)  NOT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `tbl_adressen`
+-- Tabelstructuur voor tabel tbl_klanten
 --
 
-CREATE TABLE `tbl_adressen` (
-    `a_idAdres` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `a_plaatsnaam` varchar(80) NOT NULL,
-    `a_gemeente` varchar(80) NOT NULL,
-    `a_provincie` varchar(80) NOT NULL,
-    `a_regio` varchar(80) NOT NULL,
-    `a_straatnaam` varchar(200) NOT NULL,
-    `a_huisnummer` varchar(16) NOT NULL,
-    `a_postcode` varchar(12) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE tbl_klanten
+(
+    k_idKlant     int(11)      NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    k_klantnummer varchar(24)  NOT NULL UNIQUE,
+    k_achternaam  varchar(100) NOT NULL,
+    k_voornaam    varchar(80)  NOT NULL,
+    k_fk_idAdres  int(11)      NOT NULL,
+    CONSTRAINT k_fk_idAdres FOREIGN KEY (k_fk_idAdres) REFERENCES tbl_adressen (a_idAdres)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8 COMMENT ='Klanten en verwijzing naar adres';
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `tbl_klanten`
+-- Tabelstructuur voor tabel tbl_meters
 --
 
-CREATE TABLE `tbl_klanten` (
-   `k_idKlant` int(11) NOT NULL PRIMARY KEY  AUTO_INCREMENT,
-   `k_klantnummer` varchar(24) NOT NULL UNIQUE,
-   `k_achternaam` varchar(100) NOT NULL,
-   `k_voornaam` varchar(80) NOT NULL,
-   `k_fk_idAdres` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Klanten en verwijzing naar adres';
+CREATE TABLE tbl_meters
+(
+    m_idMeter    int(11) NOT NULL PRIMARY KEY,
+    m_fk_idAdres int(11) NOT NULL,
+    m_product    CHAR(1) NOT NULL,
+    CONSTRAINT m_fk_idAdres FOREIGN KEY (m_fk_idAdres) REFERENCES tbl_adressen (a_idAdres)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8 COMMENT ='Meternummers en adressen';
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `tbl_meters`
+-- Tabelstructuur voor tabel tbl_meter_telwerken
 --
 
-CREATE TABLE `tbl_meters` (
-  `m_idMeter` int(11) NOT NULL ,
-  `m_fk_idAdres` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Meternummers en adressen';
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `tbl_meters_standen`
---
-
-CREATE TABLE `tbl_meters_standen` (
-  `ms_idMeterstand` int(11) NOT NULL PRIMARY KEY  AUTO_INCREMENT,
-  `ms_fk_idMeterTelwerk` int(11) NOT NULL,
-  `ms_stand` int(11) NOT NULL,
-  `ms_datum` date NOT NULL,
-  `ms_tijd` time NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+CREATE TABLE tbl_meter_telwerken
+(
+    mt_idMeterTelwerk int(11)     NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    mt_fk_idMeter     int(11)     NOT NULL,
+    mt_telwerk        smallint(6) NOT NULL,
+    mt_type           varchar(1)  NOT NULL COMMENT 'Verbruik of teruglevering',
+    CONSTRAINT mt_fk_idMeter FOREIGN KEY (mt_fk_idMeter) REFERENCES tbl_meters (m_idMeter)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 --
--- Tabelstructuur voor tabel `tbl_meter_telwerken`
+-- Tabelstructuur voor tabel tbl_meters_standen
 --
 
-CREATE TABLE `tbl_meter_telwerken` (
-   `mt_idMeterTelwerk` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-   `mt_fk_idMeter` int(11) NOT NULL,
-   `mt_product` varchar(1) NOT NULL,
-   `mt_telwerk` smallint(6) NOT NULL,
-   `mt_type` varchar(1) NOT NULL COMMENT 'Verbruik of teruglevering'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE tbl_meters_standen
+(
+    ms_idMeterstand      int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    ms_fk_idMeterTelwerk int(11) NOT NULL,
+    ms_stand             int(11) NOT NULL,
+    ms_datum             date    NOT NULL,
+    ms_tijd              time    NOT NULL,
+    CONSTRAINT ms_fk_idMeterTelwerk FOREIGN KEY (ms_fk_idMeterTelwerk) REFERENCES tbl_meter_telwerken (mt_idMeterTelwerk)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 --
 -- Indexen voor geëxporteerde tabellen
 --
 
 --
--- Indexen voor tabel `tbl_adressen`
+-- Indexen voor tabel tbl_adressen
 --
-ALTER TABLE `tbl_adressen`
-    ADD KEY `postcode` (`a_postcode`);
+ALTER TABLE tbl_adressen
+    ADD KEY postcode (a_postcode);
 
 --
--- Indexen voor tabel `tbl_klanten`
+-- Indexen voor tabel tbl_klanten
 --
-ALTER TABLE `tbl_klanten`
-    ADD KEY `idAdres` (`k_fk_idAdres`);
+ALTER TABLE tbl_klanten
+    ADD KEY idAdres (k_fk_idAdres);
 
 --
--- Indexen voor tabel `tbl_meters`
+-- Indexen voor tabel tbl_meters
 --
-ALTER TABLE `tbl_meters`
-    ADD KEY `idAdres` (`m_fk_idAdres`) USING BTREE;
+ALTER TABLE tbl_meters
+    ADD KEY idAdres (m_fk_idAdres) USING BTREE;
 
 --
--- Indexen voor tabel `tbl_meters_standen`
+-- Indexen voor tabel tbl_meters_standen
 --
-ALTER TABLE `tbl_meters_standen`
-    ADD KEY `idMeterTelwerk` (`ms_fk_idMeterTelwerk`);
+ALTER TABLE tbl_meters_standen
+    ADD KEY idMeterTelwerk (ms_fk_idMeterTelwerk);
 
 --
--- Indexen voor tabel `tbl_meter_telwerken`
+-- Indexen voor tabel tbl_meter_telwerken
 --
-ALTER TABLE `tbl_meter_telwerken`
-    ADD UNIQUE KEY `mt_fk_idMeter` (`mt_fk_idMeter`,`mt_telwerk`,`mt_type`,`mt_product`),
-    ADD KEY `idMeter` (`mt_fk_idMeter`);
+ALTER TABLE tbl_meter_telwerken
+    ADD UNIQUE KEY mt_fk_idMeter (mt_fk_idMeter, mt_telwerk, mt_type),
+    ADD KEY idMeter (mt_fk_idMeter);
--- database/example_queries.sql
+++ database/example_queries.sql
@@ -2,54 +2,55 @@
   * Selecteer aantal meterstanden per postcodegebied
  */
 SELECT a_postcode as postcode,
-       ms_datum as datum,
-       mt_product as product,
-       count(*) as aantalMeterstanden
+       ms_datum   as datum,
+       m_product  as product,
+       count(*)   as aantalMeterstanden
 FROM tbl_adressen
-     JOIN tbl_meters          ON m_fk_idAdres = a_idAdres
-     JOIN tbl_meter_telwerken ON mt_fk_idMeter = m_idMeter
-     JOIN tbl_meters_standen  ON ms_fk_idMeterTelwerk = mt_idMeterTelwerk
-GROUP BY a_postcode, ms_datum,mt_product;
+         JOIN tbl_meters ON m_fk_idAdres = a_idAdres
+         JOIN tbl_meter_telwerken ON mt_fk_idMeter = m_idMeter
+         JOIN tbl_meters_standen ON ms_fk_idMeterTelwerk = mt_idMeterTelwerk
+GROUP BY a_postcode, ms_datum, m_product;
 
 /**
   * Selecteer aantal meterstanden per gemeente, per maand, alleen verbruik, geen teruglevering
  */
-SELECT a_gemeente as gemeente,
+SELECT a_gemeente                     as gemeente,
        DATE_FORMAT(ms_datum, '%Y-%m') as datum,
-       mt_product as product,
-       count(*) as aantalMeterstanden
+       m_product                      as product,
+       count(*)                       as aantalMeterstanden
 FROM tbl_adressen
-    JOIN tbl_meters          ON m_fk_idAdres = a_idAdres
-    JOIN tbl_meter_telwerken ON mt_fk_idMeter = m_idMeter
-    JOIN tbl_meters_standen  ON ms_fk_idMeterTelwerk = mt_idMeterTelwerk
-WHERE mt_product in ('E','G')
-  AND mt_telwerk in (1,2)
-GROUP BY a_gemeente, datum ,mt_product;
+         JOIN tbl_meters ON m_fk_idAdres = a_idAdres
+         JOIN tbl_meter_telwerken ON mt_fk_idMeter = m_idMeter
+         JOIN tbl_meters_standen ON ms_fk_idMeterTelwerk = mt_idMeterTelwerk
+WHERE m_product in ('E', 'G')
+  AND mt_telwerk in (1, 2)
+GROUP BY a_gemeente, datum, m_product;
 
 /**
   * Selecteer aantal meterstanden per postcodegebied zonder letters
  */
-SELECT substr(a_postcode,1, 4) as postcode,
-       ms_datum as datum,
-       mt_product as product,
-       count(*) as aantalMeterstanden
+SELECT substr(a_postcode, 1, 4) as postcode,
+       ms_datum                 as datum,
+       m_product                as product,
+       count(*)                 as aantalMeterstanden
 FROM tbl_adressen
-         JOIN tbl_meters          ON m_fk_idAdres = a_idAdres
+         JOIN tbl_meters ON m_fk_idAdres = a_idAdres
          JOIN tbl_meter_telwerken ON mt_fk_idMeter = m_idMeter
-         JOIN tbl_meters_standen  ON ms_fk_idMeterTelwerk = mt_idMeterTelwerk
-GROUP BY postcode, ms_datum,mt_product;
+         JOIN tbl_meters_standen ON ms_fk_idMeterTelwerk = mt_idMeterTelwerk
+GROUP BY postcode, ms_datum, m_product;
 
 /**
   Alle meterstanden van een meter, per product in de tijd uitgezet
  */
-SELECT mt_product as product,
+SELECT m_idMeter  as meternummer,
+       m_product  as product,
        mt_telwerk as telwerk,
-       mt_type as type,
+       mt_type    as type,
        ms_stand,
        ms_datum,
        ms_tijd
-FROM tbl_meters_standen
-     JOIN tbl_meter_telwerken ON mt_idMeterTelwerk = ms_fk_idMeterTelwerk
-WHERE mt_fk_idMeter = 293058
-order by mt_product, mt_telwerk,ms_datum,
-         ms_tijd
+FROM tbl_meters
+         JOIN tbl_meter_telwerken tmt on tbl_meters.m_idMeter = tmt.mt_fk_idMeter
+         JOIN tbl_meters_standen tms on tmt.mt_idMeterTelwerk = tms.ms_fk_idMeterTelwerk
+WHERE m_idMeter = 293058
+order by m_product, mt_telwerk, ms_datum, ms_tijd
